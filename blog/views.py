d3d22d2cd59cfb015948a5595a076367883a0292
Enquil <jim.e.olesen@gmail.com>
2023-01-16 16:12:40 +0000

    add like functionality

--- blog/views.py
+++ blog/views.py
@@ -68,10 +68,13 @@ class PostDetail(View):
 
 class PostLike(View):
 
-    def post(self, request, slug):
+    def post(self, request, slug, *args, **kwargs):
+        
         post = get_object_or_404(Post, slug=slug)
 
-        if post.likes.remove(id=request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
