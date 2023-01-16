8965a6765b038e08f2af13a0cdcda1d2eabdb746
Enquil <jim.e.olesen@gmail.com>
2023-01-16 00:46:15 +0000

    add some stuff

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,9 @@
 # import the Comment model
 from .models import Comment
 from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
