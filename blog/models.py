e2bc080071c317ece3273126d70f39ed9b7a15a5
Enquil <jim.e.olesen@gmail.com>
2023-01-25 03:18:00 +0000

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Post(models.Model):
-    '''class for starting a new blogpost'''
+    '''class defining a blogpost'''
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
